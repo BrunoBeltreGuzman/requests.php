271ce2ba3479443f7945f9b9d0ee657ebd2eb35f
@BrunoDev <68261773+BrunoBeltreGuzman@users.noreply.github.com>
2021-04-21 12:24:16 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+
+<?php
+
+const codeOK = "...";
+const codePOST = "...";
+const codePUT = "...";
+const codePATCH = "...";
+const codeDELETE = "...";
+
+const messageOK = "...";
+const messagePOST = "...";
+const messagePUT = "...";
+const messagePATCH = "...";
+const messageDELETE = "...";
+
