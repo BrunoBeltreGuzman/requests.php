e32ceef35dfa08b8749cfad1a929fecc4325d0fa
@BrunoDev <68261773+BrunoBeltreGuzman@users.noreply.github.com>
2021-04-22 09:51:43 -0400

    Update parameters.php

--- parameters.php
+++ parameters.php
@@ -1,28 +1,42 @@
 <?php
 
+// Get Parameters
 function getGetParameters()
 {
        return $_GET;
 }
 
+// Post Parameters
 function getPostParameters()
 {
        return $_POST;
 }
 
+//Put Parameters
 function getPutParameters()
 {
        $PUT = null;
+       parse_str(file_get_contents("php://input"), $PUT);
        return $PUT;
 }
 
+//PaTCH Parameters
 function getPatchParameters()
 {
        $PATCH = null;
+       parse_str(file_get_contents("php://input"), $PATCH);
        return $PATCH;
 }
 
+//Delete Parameters
+function getDeleteParameters()
+{
+       $DELETE = null;
+       parse_str(file_get_contents("php://input"), $DELETE);
+       return $DELETE;
+}
 
+//Headers Parameters
 function getHeaders()
 {
        $headers = apache_request_headers();
